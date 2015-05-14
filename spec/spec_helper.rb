2c97dc0675e9352f536df0fe3ca41cddfb73e062
Arthur Santos <amsantos@thoughtworks.com>
2015-05-14 18:43:41 -0300

    Retira rake tasks que não foram implementadas

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,11 +10,4 @@ API_HOST = ENV.fetch('API_HOST', 'http://localhost:8000')
 pacto_mode = ENV['PACTO_MODE']
 require "pacto_modes/#{pacto_mode}" if pacto_mode
 
-stub_provider = ENV['STUB_WITH']
-if stub_provider
-  puts "Stubbing with: #{stub_provider}"
-  require "stub_providers/#{stub_provider}"
-else
-  puts "Running live tests"
-  WebMock.allow_net_connect!
-end
+WebMock.allow_net_connect!
