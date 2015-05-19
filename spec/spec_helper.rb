6b7979f25a340a5f9989769b1601ae49d553ab28
Arthur Santos <amsantos@thoughtworks.com>
2015-05-19 16:01:20 -0300

    Faz com que o método padrão de execução do Pacto seja a validação dos contratos

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ end
 
 API_HOST = ENV.fetch('API_HOST', 'http://localhost:8000')
 
-pacto_mode = ENV['PACTO_MODE']
+pacto_mode = ENV.fetch('PACTO_MODE', 'validate')
 require "pacto_modes/#{pacto_mode}" if pacto_mode
 
 WebMock.allow_net_connect!
