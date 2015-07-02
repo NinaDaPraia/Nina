688e7504deba13e3f47fa5a54d9a617e8e18c4c9
Vanilson <vanilson.m.santos@gmail.com>
2015-07-02 14:49:09 -0300

    Added functional test to delete a influential figure using cms as admin

--- apps/influential_figure/tests/functional/test_cms_influential_figure.py
+++ apps/influential_figure/tests/functional/test_cms_influential_figure.py
@@ -49,16 +49,27 @@ def update_influential_figure(live_server, influential_figure):
     influential_figure_link = live_server.browser.find_element_by_link_text(influential_figure.name)
     influential_figure_link.click()
 
-    expected_influential_figure_name_changed = 'New name influential figure'
+    influential_figure.name = 'New influential figure name'
     name_field = live_server.browser.find_element_by_name('name')
     name_field.clear()
-    name_field.send_keys(expected_influential_figure_name_changed)
+    name_field.send_keys(influential_figure.name)
 
     update_button = live_server.browser.find_element_by_name('_save')
     update_button.click()
 
     body = live_server.browser.find_element_by_tag_name('body')
-    live_server.assertIn('The influential figure "' + expected_influential_figure_name_changed + '" was changed successfully.', body.text)
+    live_server.assertIn('The influential figure "' + influential_figure.name + '" was changed successfully.', body.text)
+
+def delete_influential_figure(live_server, influential_figure):
+    select_influential_figure = live_server.browser.find_element_by_link_text(influential_figure.name)
+    select_influential_figure.click()
+    delete_button = live_server.browser.find_element_by_link_text('Delete')
+    delete_button.click()
+    confirm_button = live_server.browser.find_element_by_xpath('//*[@id="content"]/form/div/input[2]')
+    confirm_button.click()
+
+    body = live_server.browser.find_element_by_tag_name('body')
+    live_server.assertIn('The influential figure "' + influential_figure.name + '" was deleted successfully.', body.text)
 
 
 class FiTest(LiveServerTestCase):
@@ -80,11 +91,13 @@ class FiTest(LiveServerTestCase):
         influential_figure_app_link = self.browser.find_element_by_link_text('Influential_Figure')
         influential_figure_app_link.click()
 
-        influential_Figure = InfluentialFigure()
-        influential_Figure.name = 'TestFigure'
-        influential_Figure.description = 'This is a very important person'
-        influential_Figure.image = 'image url'
+        influential_figure = InfluentialFigure()
+        influential_figure.name = 'TestFigure'
+        influential_figure.description = 'This is a very important person'
+        influential_figure.image = 'image url'
 
-        create_influencial_figure(self, expected_social_movement, influential_Figure)
+        create_influencial_figure(self, expected_social_movement, influential_figure)
 
-        update_influential_figure(self, influential_Figure)
+        update_influential_figure(self, influential_figure)
+
+        delete_influential_figure(self, influential_figure)
