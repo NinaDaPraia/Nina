f804689e1e8ef2821c881b09cf9e341d68e63e67
Vanilson <vanilson.m.santos@gmail.com>
2015-07-02 13:39:51 -0300

    Added functional test to update a influential figure using cms as admin

--- apps/influential_figure/tests/functional/test_cms_influential_figure.py
+++ apps/influential_figure/tests/functional/test_cms_influential_figure.py
@@ -1,49 +1,64 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from apps.influential_figure.models import SocialMovement, InfluentialFigure
 
 from django.test import LiveServerTestCase
 
-def login_as_admin(liveServer, username, password):
-    liveServer.browser.get(liveServer.live_server_url + '/admin/')
-    username_field = liveServer.browser.find_element_by_name('username')
+def login_as_admin(live_server, username, password):
+    live_server.browser.get(live_server.live_server_url + '/admin/')
+    username_field = live_server.browser.find_element_by_name('username')
     username_field.send_keys('nina')
-    password_field = liveServer.browser.find_element_by_name('password')
+    password_field = live_server.browser.find_element_by_name('password')
     password_field.send_keys('nina')
     password_field.send_keys(Keys.RETURN)
 
-def create_social_moviment(liveServer, socialMovementName):
-    social_movement_model_link = liveServer.browser.find_element_by_link_text('Social movements')
+def create_social_moviment(live_server, social_movement_name):
+    social_movement_model_link = live_server.browser.find_element_by_link_text('Social movements')
     social_movement_model_link.click()
-    add_social_movement_link = liveServer.browser.find_element_by_link_text('Add social movement')
+    add_social_movement_link = live_server.browser.find_element_by_link_text('Add social movement')
     add_social_movement_link.click()
 
-    social_movement_name_field = liveServer.browser.find_element_by_name('name')
-    social_movement_name_field.send_keys(socialMovementName)
-    social_movement_save_button = liveServer.browser.find_element_by_name('_save')
+    social_movement_name_field = live_server.browser.find_element_by_name('name')
+    social_movement_name_field.send_keys(social_movement_name)
+    social_movement_save_button = live_server.browser.find_element_by_name('_save')
     social_movement_save_button.click()
 
-def create_influencial_figure(liveServer, name, socialMovement):
-    influential_figure_model_link = liveServer.browser.find_element_by_link_text('Influential figures')
+def create_influencial_figure(live_server, social_movement, influential_figure):
+    influential_figure_model_link = live_server.browser.find_element_by_link_text('Influential figures')
     influential_figure_model_link.click()
-    add_influential_figure_link = liveServer.browser.find_element_by_link_text('Add influential figure')
+    add_influential_figure_link = live_server.browser.find_element_by_link_text('Add influential figure')
     add_influential_figure_link.click()
 
-    expected_influential_figure = 'Important Figure'
-    name_field = liveServer.browser.find_element_by_name('name')
-    name_field.send_keys(expected_influential_figure)
-    description_field = liveServer.browser.find_element_by_name('description')
-    description_field.send_keys('This is a very important person')
-    image_field = liveServer.browser.find_element_by_name('image')
-    image_field.send_keys('image_url')
-    select = Select(liveServer.browser.find_element_by_name('social_movements'))
-    select.select_by_visible_text(socialMovement)
+    name_field = live_server.browser.find_element_by_name('name')
+    name_field.send_keys(influential_figure.name)
+    description_field = live_server.browser.find_element_by_name('description')
+    description_field.send_keys(influential_figure.description)
+    image_field = live_server.browser.find_element_by_name('image')
+    image_field.send_keys(influential_figure.image)
+    select = Select(live_server.browser.find_element_by_name('social_movements'))
+    select.select_by_visible_text(social_movement)
 
-    influential_figure_save_button = liveServer.browser.find_element_by_name('_save')
+    influential_figure_save_button = live_server.browser.find_element_by_name('_save')
     influential_figure_save_button.click()
 
-    body = liveServer.browser.find_element_by_tag_name('body')
-    liveServer.assertIn('The influential figure "' + expected_influential_figure + '" was added successfully.', body.text)
+    body = live_server.browser.find_element_by_tag_name('body')
+    live_server.assertIn('The influential figure "' + influential_figure.name + '" was added successfully.', body.text)
+
+def update_influential_figure(live_server, influential_figure):
+    influential_figure_link = live_server.browser.find_element_by_link_text(influential_figure.name)
+    influential_figure_link.click()
+
+    expected_influential_figure_name_changed = 'New name influential figure'
+    name_field = live_server.browser.find_element_by_name('name')
+    name_field.clear()
+    name_field.send_keys(expected_influential_figure_name_changed)
+
+    update_button = live_server.browser.find_element_by_name('_save')
+    update_button.click()
+
+    body = live_server.browser.find_element_by_tag_name('body')
+    live_server.assertIn('The influential figure "' + expected_influential_figure_name_changed + '" was changed successfully.', body.text)
 
 class FiTest(LiveServerTestCase):
 
@@ -64,4 +79,11 @@ class FiTest(LiveServerTestCase):
         influential_figure_app_link = self.browser.find_element_by_link_text('Influential_Figure')
         influential_figure_app_link.click()
 
-        create_influencial_figure(self, 'TestFigure', expected_social_movement)
+        influential_Figure = InfluentialFigure()
+        influential_Figure.name = 'TestFigure'
+        influential_Figure.description = 'This is a very important person'
+        influential_Figure.image = 'image url'
+
+        create_influencial_figure(self, expected_social_movement, influential_Figure)
+
+        update_influential_figure(self, influential_Figure)
