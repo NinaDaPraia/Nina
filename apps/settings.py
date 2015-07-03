21147f175128fb0c88397667f722bd459126f4a7
Vanilson <vanilson.m.santos@gmail.com>
2015-07-03 10:21:26 -0300

    Changed static files directories variable in settings

--- apps/settings.py
+++ apps/settings.py
@@ -100,5 +100,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
