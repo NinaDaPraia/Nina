0cde5187f9a27176efaf9baa84475da8cd3cdd32
Vanilson <vanilson.m.santos@gmail.com>
2015-07-04 22:40:11 -0300

    Added horizontal filter to social movment field in InfluentialFigure model

--- apps/influential_figure/admin.py
+++ apps/influential_figure/admin.py
@@ -4,6 +4,10 @@ from apps.influential_figure.models import SocialMovement, InfluentialFigure
 # Register your models here.
 
 admin.site.register(SocialMovement)
-admin.site.register(InfluentialFigure)
+
+class InfluentialFigureAdmin(admin.ModelAdmin):
+    filter_horizontal = ('social_movements',)
+
+admin.site.register(InfluentialFigure, InfluentialFigureAdmin)
 
 admin.site.site_header = 'Nina administration'
