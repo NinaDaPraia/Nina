26ae300fd7ff333847206a9b9a46c840f8a7d0c0
Nicolas Agustin Torres <nicolastrres@gmail.com>
2015-07-27 21:09:48 -0300

    Adding validation tests

---
+++ apps/influential_figure/tests/test_validations.py
@@ -0,0 +1,46 @@
+from rest_framework import status
+from rest_framework.test import APISimpleTestCase
+from apps.influential_figure.tests import factories
+from apps.influential_figure.tests.test_influential_figure_api import login_as_user
+
+
+class InfluentialFigureValidationTest(APISimpleTestCase):
+    def test_name_is_required(self):
+        influential_figure = factories.InfluentialFigureResource()
+        del influential_figure['name']
+        login_as_user(self)
+
+        response = self.client.post("/influential_figures", influential_figure)
+
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEquals({'name': ['This field is required.']}, response.data)
+
+    def test_description_is_required(self):
+        influential_figure = factories.InfluentialFigureResource()
+        del influential_figure['description']
+        login_as_user(self)
+
+        response = self.client.post("/influential_figures", influential_figure)
+
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEquals({'description': ['This field is required.']}, response.data)
+
+    def test_image_is_required(self):
+        influential_figure = factories.InfluentialFigureResource()
+        del influential_figure['image']
+        login_as_user(self)
+
+        response = self.client.post("/influential_figures", influential_figure)
+
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEquals({'image': ['This field is required.']}, response.data)
+
+    def test_social_movements_is_required(self):
+        influential_figure = factories.InfluentialFigureResource()
+        del influential_figure['image']
+        login_as_user(self)
+
+        response = self.client.post("/influential_figures", influential_figure)
+
+        self.assertEquals(status.HTTP_400_BAD_REQUEST, response.status_code)
+        self.assertEquals({'image': ['This field is required.']}, response.data)
